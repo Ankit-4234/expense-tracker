54151c83043193a92804f9a52f7b69d9952fb1dc
Ankit Wosti <ankitwosti2@gmail.com>
2026-06-27 15:48:23 +0000

    register page completed

--- register.php
+++ register.php
@@ -20,3 +20,29 @@ if(isset($_POST['submit'])){
         }
     }
 }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>register</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="auth_container">
+    <h1> Create account </h1>
+    <?php if(isset($error)) {?>
+    <p class="error"><?php echo $error; ?></p>
+    <?php } ?>
+    <form method="POST">
+    <input type="text" name="name" placeholder="full name" required> <br><br>
+    <input type="email" name="email" placeholder="email" required> <br> <br>
+    <input type="password" name="password" placeholder="password" required> <br> <br>
+    <input type="password" name="confirm" placeholder="confirm password" required> <br> <br>
+    <button type="submit" name="submit"> register </button>
+    </form>
+    <p>Already have an account? <a href="login.php" login> </a> </p>
+    </div>
+</body>
+</html>
